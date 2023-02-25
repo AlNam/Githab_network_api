233cd112c0e6201ea00e1fde59ee2f4b4f717ac5
AlNam <namesov1@inbox.ru>
2023-02-25 21:39:17 +0300

    test request

--- app/src/main/java/com/example/githabrepozitories/MainActivity.kt
+++ app/src/main/java/com/example/githabrepozitories/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.githabrepozitories
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.githabrepozitories.ui.theme.GithabRepozitoriesTheme
 
+const val API_KEY = "1f013a3c1f144429b3d152935232502"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GithabRepozitoriesTheme {
-
+                getData("Moscow", this)
             }
         }
     }
+}
+
+private fun getData(city: String, context: Context){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+                response ->
+            Log.d("MyLog", "Response: $response")
+        },
+        {
+            Log.d("MyLog", "VolleyError: $it")
+        }
+    )
+    queue.add(sRequest)
 }
