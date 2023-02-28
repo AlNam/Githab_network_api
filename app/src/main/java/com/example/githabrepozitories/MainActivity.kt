434cd11a6a1f323a4d5a445500ef0e39d8fdda14
AlNam <namesov1@inbox.ru>
2023-02-28 19:02:52 +0300

    request allRepo users githab MainActivity

--- app/src/main/java/com/example/githabrepozitories/MainActivity.kt
+++ app/src/main/java/com/example/githabrepozitories/MainActivity.kt
@@ -3,41 +3,50 @@ package com.example.githabrepozitories
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import com.example.githabrepozitories.screens.MainScreen
 import com.example.githabrepozitories.ui.theme.GithabRepozitoriesTheme
 
 const val API_KEY = "1f013a3c1f144429b3d152935232502"
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GithabRepozitoriesTheme {
-                getData("Moscow", this)
+                MainScreen()
             }
         }
     }
 }
 
-private fun getData(city: String, context: Context){
-    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
-            "&q=$city" +
-            "&days=" +
-            "3" +
-            "&aqi=no&alerts=no"
-    val queue = Volley.newRequestQueue(context)
-    val sRequest = StringRequest(
-        Request.Method.GET,
-        url,
-        {
-                response ->
-            Log.d("MyLog", "Response: $response")
+fun getData(courseList: MutableList<String>, ctx: Context) {
+    val url = "https://api.github.com/users/AlNam/repos"
+    val queue = Volley.newRequestQueue(ctx)
+    val sRequest = JsonArrayRequest(
+        Request.Method.GET, url, null,
+        { response ->
+            try {
+                for (i in 0 until response.length()) {
+                    val resobj = response.getJSONObject(i)
+                    val repo = resobj.getString("name")
+
+                    courseList += repo
+                    Log.d("MyLog", "Response: $repo")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         },
-        {
-            Log.d("MyLog", "VolleyError: $it")
+        { error ->
+            // in this case we are simply displaying a toast message.
+            Toast.makeText(ctx, "Fail to get response", Toast.LENGTH_SHORT)
+                .show()
         }
     )
     queue.add(sRequest)
