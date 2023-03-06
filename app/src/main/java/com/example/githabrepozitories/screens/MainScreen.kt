75acdc14c62189d517b1623a2219eddac3f65415
AlNam <namesov1@inbox.ru>
2023-03-06 21:50:30 +0300

    search name repo githab

--- app/src/main/java/com/example/githabrepozitories/screens/MainScreen.kt
+++ app/src/main/java/com/example/githabrepozitories/screens/MainScreen.kt
@@ -1,73 +1,85 @@
 package com.example.githabrepozitories.screens
 
 
-import androidx.compose.foundation.Image
+import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.example.githabrepozitories.R
-import com.example.githabrepozitories.getData
+import androidx.compose.ui.unit.sp
+import com.example.githabrepozitories.data.RepoModel
 import com.example.githabrepozitories.ui.theme.Bluelight
 
 
-@Preview(showBackground = true)
-@Composable
-fun MainScreen() {
-    Image(
-        painter = BitmapPainter(ImageBitmap.imageResource(R.drawable.strokes_blue_brown_paint)),
-        contentDescription = "im1",
-        modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.5f),
-        contentScale = ContentScale.FillBounds
 
-    )
+@Composable
+fun MainScreen(ctx: Context, repolist: MutableState<List<RepoModel>>,nameRepo: MutableState<String>) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp)
     ) {
-        Card(modifier = Modifier.fillMaxWidth(),
+        Card(
+            modifier = Modifier.fillMaxWidth(),
             backgroundColor = Bluelight,
             elevation = 0.dp,
             shape = RoundedCornerShape(10.dp)
         ) {
-            displayListView()
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = nameRepo.value,
+                    textStyle = TextStyle(fontSize=25.sp),
+                    onValueChange = {newText -> nameRepo.value = newText},
+                    placeholder = { Text(text = "InputnameRepoGithab")}
+                )
+                InputItemCard(ctx, repolist)
+            }
         }
     }
 }
 
 @Composable
-fun displayListView() {
-    val context = LocalContext.current
-    val repoList = remember {
-        mutableStateListOf<String>()
-    }
-    getData(repoList, context)
-    LazyColumn {
-        items(repoList) {
-            repo ->
-            Text(text = repo, modifier = Modifier.padding(15.dp))
-            Divider()
+fun InputItemCard(ctx:Context, repolist: MutableState<List<RepoModel>>) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        backgroundColor = Bluelight,
+        elevation = 0.dp,
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            itemsIndexed(
+                repolist.value
+                /*listOf(
+                    RepoModel("repoName"),
+                    RepoModel("repoName")
+                )*/
+            ) {index, item ->
+                ListItem(item = item)
+            }
         }
     }
 }
+
+@Composable
+fun ListItem(item: RepoModel) {
+    Text(text = item.repoName)
+}
+
+
